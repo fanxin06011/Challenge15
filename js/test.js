faf7c33120b0e3f7775b46031545170cad12e0a3
ChemyHusky <ChemyHusky@sjtu.edu.cn>
2015-11-07 12:04:26 +0800

    change

--- js/test.js
+++ js/test.js
@@ -7,7 +7,7 @@ function View3(Observer){
 	function init(){
 		margin = {top: 30, right: 10, bottom: 10, left: 10};
 		width = 1000 - margin.left - margin.right;
-		height = 800 - margin.top - margin.bottom;
+		height = 500 - margin.top - margin.bottom;
 
 		x = d3.scale.ordinal().rangePoints([0, width], 1);
 		y = {};
