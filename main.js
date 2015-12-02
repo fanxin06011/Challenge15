5cc820ca88bde31114f4307a1a77317c632421e1
ChemyHusky <ChemyHusky@sjtu.edu.cn>
2015-12-02 20:55:17 +0800

    need

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 function (){
 	var obs = Observer();
 
-	/*var view1a = View1a(obs);
+	var view1a = View1a(obs);
 	var view1b = View1b(obs);
-	var view2 = View2(obs);*/
+	var view2 = View2(obs);
 	var view3 = View3(obs);
 	var view4 = View4(obs);
 	var view5 = View5(obs);
